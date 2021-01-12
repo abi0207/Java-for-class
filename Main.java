a906d992d1caa884375a754f99df26035fc1d12c
abi0207 <77313152+abi0207@users.noreply.github.com>
2021-01-12 12:01:05 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextInt()) {
+            int i, n = sc.nextInt();
+            for (i = 2; i <= n; ++i)
+                if (n % i == 0) {
+                    System.out.print(i);
+                    n /= i;
+                    break;
+                }
+            for (; i <= n; ++i)
+                while (i <= n)
+                    if (n % i == 0) {
+                        System.out.print(" " + i);
+                        n /= i;
+                    } else
+                        break;
+            System.out.println();
+        }
+    }
+}
