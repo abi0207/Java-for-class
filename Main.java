7212258cdaf70d5d0d76fad3c676b604d3e6b7c8
abi0207 <77313152+abi0207@users.noreply.github.com>
2021-01-12 11:58:59 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,23 +2,34 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        while(sc.hasNextInt()){
-            int i,n = sc.nextInt();
-            for(i=2; i<=n; ++i)
-                if(n%i==0){
-                    System.out.print(i);
-                    n/=i;
-                    break;
-                }
-            for(; i<=n; ++i)
-                while(i<=n)
-                    if(n%i==0){
-                        System.out.print(" "+i);
-                        n/=i;
+        while (sc.hasNextInt()) {
+            int n = sc.nextInt();
+            if (n % 2 == 0)
+                System.out.println("It is not an odd number.");
+            else {
+                System.out.println((n * n / 2 + 1) * n);
+                int i = n * n, j, y = n / 2 + 1, x = 1;
+                int[][] a = new int[105][105];
+                for (; i > 0; --i) {
+                    if (a[(x + n - 1) % n][(y + n - 1) % n] == 0) {
+                        x = (x + n - 1) % n;
+                        y = (y + n - 1) % n;
+                        a[x][y] = i;
+                    } else {
+                        x = (x + 1) % n;
+                        a[x][y] = i;
                     }
-                    else
-                        break;
-            System.out.println();
+                }
+                for (i = 0; i < n; ++i) {
+                    for (j = 0; j < n; ++j)
+                        System.out.printf("%5d", a[i][j]);
+                    System.out.println();
+                }
+            }
+            if (sc.hasNextInt())
+                System.out.println();
+            else
+                break;
         }
     }
-}  
+}
